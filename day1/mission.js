bd2d6d16e8fbb16c564b320914bcc53a0de9c0c6
jm <jm.dev.study@gmail.com>
2021-11-02 14:32:13 +0900

    getArea 함수를 분리하여 getCircle, getRectangle, getTrapezoid 함수를 생성

--- day1/mission.js
+++ day1/mission.js
@@ -1,32 +1,41 @@
 // getArea 함수 만들기
-function getArea(shape, width, length, height) {
-  switch (shape) {
-    case 'circle':
-      if (length !== undefined) {
-        let result = 0;
-        for (let i = 0; i < length; i++) {
-          result += Math.trunc((width + i) * (width + i) * Math.PI);
-        } 
-        console.log(result);
-      } else {
-        console.log(Math.trunc(width * width * Math.PI)); 
-      }
-      break;
-    case 'rect':
-      console.log(width * length); 
-      break;
-    case 'trapezoid':
-      console.log(0.5 * (width + length) * height);
-      break;
+function getArea(shape, ...size) {
+  if (shape === 'circle') {
+    return getCircle(...size)
+  } else if (shape === 'rect') {
+    return getRectangle(...size)
+  } if (shape === 'trapezoid') {
+    return getTrapezoid(...size)
   }
 }
 
+function getCircle(radius, total) {
+  if (total !== undefined) {
+    let result = 0;
+    for (let i = 0; i < total; i++) {
+      result += Math.trunc((radius + i) * (radius + i) * Math.PI);
+    } 
+    return result;
+  } else {
+    return Math.trunc(radius * radius * Math.PI); 
+  }
+}
+
+function getRectangle(width, length) {
+  const result = width * length; 
+  return result;
+}
+
+function getTrapezoid(upper, lower, height) {
+  const result = 0.5 * (upper + lower) * height;
+  return result;
+}
 
 // 함수 출력
-getArea('circle', 10);
-getArea('rect', 10, 15);
-getArea('trapezoid', 10, 15, 12);
-getArea('circle', 2, 3);
+console.log(getArea('circle', 10));
+console.log(getArea('rect', 10, 15));
+console.log(getArea('trapezoid', 10, 15, 12));
+console.log(getArea('circle', 2, 3));
 
 // printExecutionSequence
 function printExecutionSequence() {
