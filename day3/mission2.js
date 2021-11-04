c0e0774ed97f83ef8d76b9e0a31902b308cc8fea
jm <jm.dev.study@gmail.com>
2021-11-04 17:54:59 +0900

    [미션2] 4. 배열 만들기 구현

--- day3/mission2.js
+++ day3/mission2.js
@@ -1,4 +1,4 @@
-// 1. factorial 함수
+// ----------------- 1. factorial 함수 -----------------
 function factorial(n) {
   const factorial = [];
 
@@ -14,18 +14,16 @@ function factorial(n) {
 console.log(factorial(4));
 
 
-// 2. 배열 거르기: 정규식, test, replace
+// ----------------- 2. 배열 거르기: 정규식, test, replace -----------------
 const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
+const regCharNum = /^[a-zA-z0-9]*$/;
 
 // for문 사용
 function filterId1(peoples) {
-  const regCharNum = /^[a-zA-z0-9]*$/;
-  const regNum = /[0-9]/;
-
   const result = [];
   for (let i = 0; i < peoples.length; i++) {
-    if (regCharNum.test(peoples[i]) === true) {
-      result.push(peoples[i].replace(regNum, ''));
+    if (regCharNum.test(peoples[i])) {
+      result.push(peoples[i].replace(/[0-9]/, ''));
     }
   }
   return result;
@@ -35,19 +33,16 @@ console.log(filterId1(peoples));
 
 // 고차함수 사용: filter. map
 function filterId2(peoples) {
-  const regCharNum = /^[a-zA-z0-9]*$/;
-  const regNum = /[0-9]/;
-
   const result = peoples
   .filter(filter => regCharNum.test(filter))
-  .map(rmNum => rmNum.replace(regNum, ''));
+  .map(rmNum => rmNum.replace(/[0-9]/, ''));
 
   return result;
 }
 
 console.log(filterId2(peoples));
 
-// 3. 평균 구하기 : 다차원 배열, reduce
+// ----------------- 3. 평균 구하기 : 다차원 배열, reduce -----------------
 const grades = [[88,76,77], [33,44,44], [90,100,94], [30,44,98]];
 
 // 배열의 평균을 구하는 함수
@@ -77,3 +72,46 @@ function getMaxAvg(grades) {
 }
 
 console.log(getMaxAvg(grades));
+
+// ----------------- 4. 배열 만들기 -----------------
+const data = {
+  "debug": "on",
+  "window": {
+      "title": "Sample Konfabulator Widget",
+      "name": "main_window",
+      "width": 500,
+      "height": 500
+  },
+  "image": { 
+      "src": "Images/Sun.png",
+      "name": "sun1",
+      "hOffset": 250,
+      "vOffset": 250,
+      "alignment": "center"
+  },
+  "text": {
+      "data": "Click Here",
+      "size": 36,
+      "style": "bold",
+      "name": "text1",
+      "hOffset": 250,
+      "vOffset": 100,
+      "alignment": "center",
+      "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+  }
+}
+
+function NumberTypeData(data) {
+  const arr = [];
+
+  for (const key in data) {
+    Object.entries(data[key]).map(([key, value]) => {
+      if (typeof value === 'number') {
+        arr.push(key);
+      } 
+    });
+  }
+  return arr;
+}
+
+console.log(NumberTypeData(data));
