919a85855e2df8227a212d0befe0b82e5da02777
jm <jm.dev.study@gmail.com>
2021-11-22 23:56:50 +0900

    [JS] 1. mvc 패턴으로 클래스 분할 2. View 클래스와 Controller 클래스를 사용하여 아이템 추가/삭제/체크박스 구현

---
+++ Week3_Wed/mvc.js
@@ -0,0 +1,104 @@
+// 로컬 스토리지 관련
+class Model {
+  constructor() {
+    this.todoList = [];
+  }
+
+  loadToDo() {
+
+  }
+
+  saveToDos() {
+
+  }
+
+  newToDoObj() {
+
+  }
+
+  doneToDos() {
+
+  }
+
+  removeToDos() {
+
+  }
+}
+
+// UI 관련 (DOM)
+class View {
+  constructor() {
+    this.element = document.querySelector('#todo_form');
+    this.input = document.querySelector('#form_input');
+    this.itemLists = document.querySelector('#todo_contents');
+  }
+
+  addList(inputValue) {
+    if (inputValue  === '') return alert('입력하세요');
+
+    const addNewItem = this.createItem(inputValue);
+    this.itemLists.appendChild(addNewItem);
+  }
+
+  createItem(inputValue) {
+    const newItem = document.createElement('li');
+    newItem.setAttribute('class', 'todo_item');
+    newItem.innerHTML = `
+    <button class="item_checkBtn">
+    <i class="far fa-square checkIcon"></i>
+    </button>
+    <span class="item_text" data-done=false>${inputValue}</span> 
+    <button class="item_deleteBtn">
+    <i class="fas fa-minus-circle deleteIcon"></i>
+    </button>
+    `;
+
+    return newItem
+  }
+
+  checkBtnOnOff() {
+    e.classList.toggle('fa-square');
+    e.classList.toggle('fa-check-square');
+  }
+
+  removeList(e) {
+    e.closest('.todo_item').remove();
+  }
+}
+
+// 이벤트 핸들러, init 
+class Controller {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  init() {
+    this.formSubmitHandler();
+    this.btnClickHandler();
+  }
+
+  formSubmitHandler() {
+    this.view.element.addEventListener('submit', (e) => this.submitHandle(e));
+  }
+
+  submitHandle(e) {
+    e.preventDefault();
+    this.view.addList(this.view.input.value);
+    this.view.input.value = "";
+  }
+
+  btnClickHandler() {
+    this.view.itemLists.addEventListener('click', e => {
+      if (e.target.classList.contains('checkIcon')) this.view.checkBtnOnOff(e.target);
+
+      if (e.target.classList.contains('deleteIcon')) this.view.removeList(e.target);
+    })
+  }
+}
+
+
+const model = new Model();
+const view = new View();
+const todoList = new Controller(model, view);
+todoList.init();
