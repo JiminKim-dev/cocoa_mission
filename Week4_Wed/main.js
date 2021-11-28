f35daedaa933f180bcf0d9b8ae4ca4d14d99f14f
jm <jm.dev.study@gmail.com>
2021-11-29 01:12:48 +0900

    [JS] 'mousemove' 이벤트 시 호출 횟수를 제한(스로틀) 하도록 설정

--- Week4_Wed/main.js
+++ Week4_Wed/main.js
@@ -2,21 +2,43 @@ class DropdownController {
   constructor() {
     this.menu = document.querySelector('.dropdown-menu');
     this.showMenu = document.querySelector('.dropdown-toggle');
-    this.fruitids = document.querySelectorAll('.count-fruit');
+    this.countFruits = document.querySelector('.count-fruits');
+
+    this.debounceTimer;
+    this.trottleTimer;
+  }
+
+  init() {
+    this.showFruitMenu();
+    this.showCountFruits();
   }
 
   showFruitMenu() {
-    let timer;
     this.showMenu.addEventListener('mouseenter', () => {
-      timer = setTimeout(() => this.menu.classList.toggle('show'), 1000);
+      this.debounceTimer = setTimeout(() => this.menu.classList.toggle('show'), 1000);
     });
 
     this.showMenu.addEventListener('mouseleave', () => {
-      clearTimeout(timer);
+      clearTimeout(this.debounceTimer);
     });
   }
 
+  showCountFruits() {
+    this.menu.addEventListener('mousemove', (e) => {
+      if (!this.trottleTimer) {
+        this.trottleTimer = setTimeout(() => {
+          this.renderCountFruits(e)
+          this.trottleTimer = null;
+        }, 500)
+      }
+    })
+  }
+
+  renderCountFruits(e) {
+    // 마우스가 움직일때마다 카운트하는 UI 추가
+    console.log(e.target.id);
+  }
 }
 
 const smartDropdown = new DropdownController()
-smartDropdown.showFruitMenu();
+smartDropdown.init();
