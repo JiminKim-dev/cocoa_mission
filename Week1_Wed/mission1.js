677a2260c9357ab0d001ffd5606ff2bcde4e3c63
jm <jm.dev.study@gmail.com>
2021-12-16 22:54:39 +0900

    [JS] 문제 1의 solution 함수 수정, 문제2 구현 - 함수 solution의 매개변수와 변수명을 수정하고 함수 findPlayerAnswer와 printMessage의 결과값을 반환하도록 수정했다. - 함수 findPlayerAnswer는 특정한 차례에 있는 플레이어가 말해야 하는 숫자 배열을 반환한다.

--- Week1_Wed/mission1.js
+++ Week1_Wed/mission1.js
@@ -1,12 +1,35 @@
 // 문제1) T개의 숫자까지 M명이 말한다고 할때 이를 모두 출력하는 프로그램을 만든다.
-function solution(notation, t, m) {
-  const printNum = [];
-  for(let i = 0; i < t * m; i++) {
-    printNum.push(...i.toString(notation))
+function solution(notation, number, totalPlayer, playerNum) {
+  const num = [];
+  
+  for(let i = 0; i < number * totalPlayer; i++) {
+    num.push(...i.toString(notation))
   }
-  return printNum
+
+  const gildongAnswer = findPlayerAnswer(num, totalPlayer, playerNum);
+  const result = printMessage(notation, number, totalPlayer, playerNum, gildongAnswer);
+
+  return result
 }
-console.log(solution(2, 4, 2));
 
 // 문제2) 길동이 차례 숫자 맞추기: 홍길동의 차례에 어떤 숫자를 말해야 하는지를 알 수 있는 프로그램을 만든다.
+function findPlayerAnswer(numArr, totalPlayer, playerNum) {
+  const answer = [];
 
+  for (let j = playerNum - 1; j < numArr.length; j++) {
+    if (j % totalPlayer === playerNum - 1) {
+      answer.push(numArr[j]);
+    }
+  }
+  return answer
+}
+
+function printMessage(notation, number, totalPlayer, playerNum, gildongAnswer) {
+  const message = `${notation}진법으로 ${totalPlayer}명당 ${number}개의 숫자까지 말한다고 했을 때 ${playerNum}번째에 있는 길동이가 말할 숫자는 ${gildongAnswer}입니다. `;
+
+  return message
+}
+
+console.log(solution(2, 4, 2, 2));
+console.log(solution(8, 6, 2, 2));
+console.log(solution(16, 4, 3, 2));
